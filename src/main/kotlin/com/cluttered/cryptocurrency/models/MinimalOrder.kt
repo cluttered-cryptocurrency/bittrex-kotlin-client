65559b44515082065c9fad1464b66c0ce4819bc1
David Clutter <cluttered.code@gmail.com>
2017-11-25 21:32:01 -0700

    Add Type to minimalOrder

--- src/main/kotlin/com/cluttered/cryptocurrency/models/MinimalOrder.kt
+++ src/main/kotlin/com/cluttered/cryptocurrency/models/MinimalOrder.kt
@@ -3,6 +3,7 @@ package com.cluttered.cryptocurrency.models
 import com.google.gson.annotations.SerializedName
 
 data class MinimalOrder(
+        @SerializedName("Type") val type: Int,
         @SerializedName("Quantity") val quantity: Double,
         @SerializedName("Rate") val rate: Double
 )
