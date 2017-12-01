77bc561d0b6d52507582c01f2d5e282c4c9c84ba
David Clutter <cluttered.code@gmail.com>
2017-12-01 01:42:46 -0700

    start adding signalr

---
+++ src/main/kotlin/com/cluttered/cryptocurrency/Slf4jLogger.kt
@@ -0,0 +1,20 @@
+package com.cluttered.cryptocurrency
+
+import donky.microsoft.aspnet.signalr.client.LogLevel
+import donky.microsoft.aspnet.signalr.client.Logger
+import org.slf4j.LoggerFactory
+
+class Slf4jLogger : Logger {
+
+    companion object {
+        private val LOG: org.slf4j.Logger = LoggerFactory.getLogger("donkey.mircrosoft.aspnet.signalr")
+    }
+
+    override fun log(message: String, level: LogLevel) {
+        when (level) {
+            LogLevel.Critical -> LOG.error(message)
+            LogLevel.Information -> LOG.info(message)
+            LogLevel.Verbose -> LOG.debug(message)
+        }
+    }
+}
