fa2a28f224b25c4a9a00a2186379e80d2ddd2d2a
David Clutter <cluttered.code@gmail.com>
2017-12-20 14:24:19 -0700

    add more to market and remove unused class

--- src/main/kotlin/com/cluttered/cryptocurrency/model/Market.kt
+++ src/main/kotlin/com/cluttered/cryptocurrency/model/Market.kt
@@ -5,14 +5,14 @@ import com.google.gson.annotations.SerializedName
 data class Market(
         @SerializedName("MarketName") val marketName: String,
         @SerializedName("IsActive") val isActive: Boolean,
-        @SerializedName("MinTradeSize") val minTradeSize: Double
+        @SerializedName("MinTradeSize") val minTradeSize: Double,
 
-//        @SerializedName("MarketCurrency") val marketCurrency: String,
-//        @SerializedName("MarketCurrencyLong") val marketCurrencyLong: String,
-//
-//        @SerializedName("BaseCurrency") val baseCurrency: String,
-//        @SerializedName("BaseCurrencyLong") val baseCurrencyLong: String,
-//
+        @SerializedName("MarketCurrency") val marketCurrency: String,
+        @SerializedName("MarketCurrencyLong") val marketCurrencyLong: String,
+
+        @SerializedName("BaseCurrency") val baseCurrency: String,
+        @SerializedName("BaseCurrencyLong") val baseCurrencyLong: String
+
 //        @SerializedName("Notice") val notice: String,
 //        @SerializedName("IsSponsored") val isSponsored: Boolean,
 //        @SerializedName("Created") val created: ZonedDateTime,
