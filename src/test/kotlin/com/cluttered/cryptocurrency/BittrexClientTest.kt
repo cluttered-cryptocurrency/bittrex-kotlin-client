8109f12a09c5b584ca5f2e4d2d8e38d74509d868
David Clutter <cluttered.code@gmail.com>
2017-12-20 14:10:17 -0700

    add client test

--- src/test/kotlin/com/cluttered/cryptocurrency/BittrexClientTest.kt
+++ src/test/kotlin/com/cluttered/cryptocurrency/BittrexClientTest.kt
@@ -1,14 +1,29 @@
 package com.cluttered.cryptocurrency
 
-import junit.framework.Assert.assertTrue
+import com.cluttered.cryptocurrency.services.PublicBittrexService
+import io.reactivex.Observable
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 
 
 class BittrexClientTest {
 
+    private val publicBittrexService by lazy {
+        PublicBittrexService.create()
+    }
+
+
     @Test
     fun testTrue() {
-        assertTrue(true)
+        val expectedMarketName = "BTC-ETH"
+        var result = ""
+        publicBittrexService.getMarkets()
+                .map { it.result }
+                .flatMap { Observable.fromIterable(it) }
+                .filter { it.marketName == expectedMarketName }
+                .subscribe { result = it.marketName}
+
+        assertThat(result).isEqualTo(expectedMarketName)
     }
 
 }
