61bab372173ff73beaa2ada6b4b79c0fce36cf42
David Clutter <cluttered.code@gmail.com>
2017-12-21 16:01:18 -0700

    Added Examples

--- src/main/kotlin/com/cluttered/cryptocurrency/examples/PublicBittrexClientExample.kt
+++ src/main/kotlin/com/cluttered/cryptocurrency/examples/PublicBittrexClientExample.kt
@@ -36,5 +36,44 @@ object PublicBittrexClientExample {
                 .filter { it.success }
                 .flatMap { Observable.fromIterable(it.result) }
                 .subscribe { println(it) }
+
+        // Individual 24 hour Market Summary
+        println("\n\n################ BTC-ETH Market Summaries ################")
+        bittrexClient.public.getMarketSummary("BTC-ETH")
+                .filter { it.success }
+                .flatMap { Observable.fromIterable(it.result) }
+                .firstElement()
+                .subscribe { println(it) }
+
+        // Order Book
+        println("\n\n################ BTC-ETH Order Book ################")
+        val orderBookObservable = bittrexClient.public.getOrderBook("BTC-ETH")
+                .filter { it.success }
+                .map { it.result }
+        orderBookObservable.flatMap { Observable.fromIterable(it.buy) }
+                .subscribe { println("Buy - $it") }
+        orderBookObservable.flatMap { Observable.fromIterable(it.sell) }
+                .subscribe { println("Sell - $it") }
+
+        // Buy Order Book
+        println("\n\n################ BTC-ETH Buy Order Book ################")
+        bittrexClient.public.getBuyOrderBook("BTC-ETH")
+                .filter { it.success }
+                .flatMap { Observable.fromIterable(it.result) }
+                .subscribe { println(it) }
+
+        // Sell Order Book
+        println("\n\n################ BTC-ETH Sell Order Book ################")
+        bittrexClient.public.getSellOrderBook("BTC-ETH")
+                .filter { it.success }
+                .flatMap { Observable.fromIterable(it.result) }
+                .subscribe { println(it) }
+
+        // Market History
+        println("\n\n################ BTC-ETH Market History ################")
+        bittrexClient.public.getMarketHistory("BTC-ETH")
+                .filter { it.success }
+                .flatMap { Observable.fromIterable(it.result) }
+                .subscribe { println(it) }
     }
 }
