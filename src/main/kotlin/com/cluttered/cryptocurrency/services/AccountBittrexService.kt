9989e16bb76cd53983e7f45d874b8df5fa4c94c1
David Clutter <cluttered.code@gmail.com>
2017-12-22 02:04:38 -0700

    add get balance

--- src/main/kotlin/com/cluttered/cryptocurrency/services/AccountBittrexService.kt
+++ src/main/kotlin/com/cluttered/cryptocurrency/services/AccountBittrexService.kt
@@ -5,11 +5,13 @@ import com.cluttered.cryptocurrency.model.Balance
 import io.reactivex.Observable
 import retrofit2.Retrofit
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface AccountBittrexService {
 
     companion object {
         const val V1_ACCOUNT = "v1.1/account"
+        const val CURRENCY = "currency"
 
         fun create(retrofit: Retrofit): AccountBittrexService {
             return retrofit.create(AccountBittrexService::class.java)
@@ -18,4 +20,7 @@ interface AccountBittrexService {
 
     @GET("$V1_ACCOUNT/getbalances")
     fun getBalances(): Observable<ApiResponse<List<Balance>>>
+
+    @GET("$V1_ACCOUNT/getbalance")
+    fun getBalance(@Query(CURRENCY) currency: String): Observable<ApiResponse<Balance>>
 }
