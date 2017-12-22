adb77b34ebce8d1e4f19f6282ec7fb753944a752
David Clutter <cluttered.code@gmail.com>
2017-12-22 02:16:44 -0700

    better commenting in example

--- src/main/kotlin/com/cluttered/cryptocurrency/examples/BittrexClientExample.kt
+++ src/main/kotlin/com/cluttered/cryptocurrency/examples/BittrexClientExample.kt
@@ -11,22 +11,30 @@ object BittrexClientExample {
 
     @JvmStatic
     fun main(args: Array<String>) {
+
+        // List Open Orders
+        println("\n\n################ OpenOrders ################")
         bittrexClient.market.getOpenOrders()
                 .filter { it.success }
                 .flatMap { Observable.fromIterable(it.result) }
                 .subscribe { println(it) }
 
-
+        // List Open Orders on Market
+        println("\n\n################ BTC-ETH OpenOrders ################")
         bittrexClient.market.getOpenOrders("BTC-ETH")
                 .filter { it.success }
                 .flatMap { Observable.fromIterable(it.result) }
                 .subscribe { println(it) }
 
+        // List Markets
+        println("\n\n################ Markets ################")
         bittrexClient.public.getMarkets()
                 .filter { it.success }
                 .flatMap { Observable.fromIterable(it.result) }
                 .subscribe { println(it) }
 
+        // List Balances
+        println("\n\n################ Balances ################")
         bittrexClient.account.getBalances()
                 .filter { it.success }
                 .flatMap { Observable.fromIterable(it.result) }
